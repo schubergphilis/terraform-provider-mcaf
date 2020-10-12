b7443d0c76519c22dfc533194fb35d6bd7f44615
Stephen Hoekstra <shoekstra@schubergphilis.com>
2020-10-13 00:28:11 +0200

    Move mcaf -> internal/mcaf
    Signed-off-by: Stephen Hoekstra <shoekstra@schubergphilis.com>

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/schubergphilis/terraform-provider-mcaf/mcaf"
+	"github.com/schubergphilis/terraform-provider-mcaf/internal/mcaf"
 )
 
 func main() {
