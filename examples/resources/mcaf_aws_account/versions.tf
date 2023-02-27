364305ebcccd1832b31eeb392f05f944437cbae6
Júlio Pedrosa <julio.alexandre.pedrosa@gmail.com>
2023-02-27 18:23:01 +0100

    Adding Close account feature and flag attribute to close or not #19

--- examples/resources/mcaf_aws_account/versions.tf
+++ examples/resources/mcaf_aws_account/versions.tf
@@ -1,8 +1,7 @@
 terraform {
   required_providers {
     mcaf = {
-      version = "0.4.3"
-      source = "provider-development.local/schubergphilis/mcaf"
+      source = "schubergphilis/mcaf"
     }
   }
 }
